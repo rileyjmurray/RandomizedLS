f8761f49898015b4571ceebb11c16ee6a5d9a2bc
Haoyun Li <haoyunl2@berkeley.edu>
2021-09-29 01:46:35 -0700

    Update large_test_for_diff_phase_time_diff_matrix_cond.py

--- Haoyun/randomized_least_square_solver/Test/large_test_for_diff_phase_time_diff_matrix_cond.py
+++ Haoyun/randomized_least_square_solver/Test/large_test_for_diff_phase_time_diff_matrix_cond.py
@@ -1,8 +1,8 @@
 import csv
 import numpy as np
-from ..Blendenpik.Riley_Blendenpik_new import blendenpik_srct
+from Haoyun.randomized_least_square_solver.Blendenpik.Riley_Blendenpik_new import blendenpik_srct
 from test_matrix_generator import overdetermined_ls_test_matrix_generator
-from ..LSRN.LSRN_over import LSRN_over
+from Haoyun.randomized_least_square_solver.LSRN.LSRN_over import LSRN_over
 
 # Set the tolerance to be 1e-12
 tol = 1e-12
